b53d7c5e531b94366d1922deea4d138bf9f0fc0d
Josh Quinzon <quinzonjosh261@gmail.com>
2024-07-13 13:50:13 +0800

    Removed Unnecessary Packages
    removed uneccessary packages in registration.java

--- CSSECDV MP/src/View/Register.java
+++ CSSECDV MP/src/View/Register.java
@@ -2,19 +2,9 @@
 package View;
 
 import Controller.PasswordHasher;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class Register extends javax.swing.JPanel {
