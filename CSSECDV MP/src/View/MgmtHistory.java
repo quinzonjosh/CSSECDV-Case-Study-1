d8f48f728f5815cc44da7a5c9049502b78403d37
Josh Quinzon <quinzonjosh261@gmail.com>
2024-07-30 18:36:34 +0800

    Implemented Manager Side History Display
    Manager can now see purchase history of all users

--- CSSECDV MP/src/View/MgmtHistory.java
+++ CSSECDV MP/src/View/MgmtHistory.java
@@ -55,7 +55,8 @@ public class MgmtHistory extends javax.swing.JPanel {
             }
             
     //      LOAD CONTENTS
-            ArrayList<History> history = sqlite.getHistory(current.getUsername());
+            ArrayList<History> history = (current.getRole() == 2) ? sqlite.getHistory(current.getUsername()) : (current.getRole() == 4) ? sqlite.getHistory() : null;
+
             for(int nCtr = 0; nCtr < history.size(); nCtr++){
                 
                 Product product = sqlite.getProduct(history.get(nCtr).getName());
@@ -191,18 +192,18 @@ public class MgmtHistory extends javax.swing.JPanel {
 //          LOAD CONTENTS
             ArrayList<History> history = sqlite.getHistory();
             for(int nCtr = 0; nCtr < history.size(); nCtr++){
-                if(searchFld.getText().contains(history.get(nCtr).getUsername()) || 
-                   history.get(nCtr).getUsername().contains(searchFld.getText()) || 
-                   searchFld.getText().contains(history.get(nCtr).getName()) || 
+                if(searchFld.getText().contains(history.get(nCtr).getUsername()) ||
+                   history.get(nCtr).getUsername().contains(searchFld.getText()) ||
+                   searchFld.getText().contains(history.get(nCtr).getName()) ||
                    history.get(nCtr).getName().contains(searchFld.getText())){
-                
+
                     Product product = sqlite.getProduct(history.get(nCtr).getName());
                     tableModel.addRow(new Object[]{
-                        history.get(nCtr).getUsername(), 
-                        history.get(nCtr).getName(), 
-                        history.get(nCtr).getStock(), 
-                        product.getPrice(), 
-                        product.getPrice() * history.get(nCtr).getStock(), 
+                        history.get(nCtr).getUsername(),
+                        history.get(nCtr).getName(),
+                        history.get(nCtr).getStock(),
+                        product.getPrice(),
+                        product.getPrice() * history.get(nCtr).getStock(),
                         history.get(nCtr).getTimestamp()
                     });
                 }
