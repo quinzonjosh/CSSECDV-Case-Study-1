ef902deed31b208de301ba1657421ad881cc5d20
Josh Quinzon <quinzonjosh261@gmail.com>
2024-08-01 18:52:47 +0800

    Modified implementation of client and manager searching for products and usernames
    managers can now search for any product or username while clients can search for any products under their username

--- CSSECDV MP/src/View/MgmtHistory.java
+++ CSSECDV MP/src/View/MgmtHistory.java
@@ -201,23 +201,44 @@ public class MgmtHistory extends javax.swing.JPanel {
 
 //          LOAD CONTENTS
             ArrayList<History> history = sqlite.getHistory();
-            for(int nCtr = 0; nCtr < history.size(); nCtr++){
-                if(searchFld.getText().contains(history.get(nCtr).getUsername()) ||
-                   history.get(nCtr).getUsername().contains(searchFld.getText().toLowerCase()) ||
-                   searchFld.getText().contains(history.get(nCtr).getName()) ||
-                   history.get(nCtr).getName().contains(searchFld.getText().toLowerCase())){
 
-                    Product product = sqlite.getProduct(history.get(nCtr).getName());
-                    tableModel.addRow(new Object[]{
-                        history.get(nCtr).getUsername(),
-                        history.get(nCtr).getName(),
-                        history.get(nCtr).getStock(),
-                        product.getPrice(),
-                        product.getPrice() * history.get(nCtr).getStock(),
-                        history.get(nCtr).getTimestamp()
-                    });
+            if(current.getRole() == 4){
+                for(int nCtr = 0; nCtr < history.size(); nCtr++){
+                    if(searchFld.getText().contains(history.get(nCtr).getUsername()) ||
+                            history.get(nCtr).getUsername().contains(searchFld.getText()) ||
+                            searchFld.getText().contains(history.get(nCtr).getName()) ||
+                            history.get(nCtr).getName().contains(searchFld.getText())){
+
+                        Product product = sqlite.getProduct(history.get(nCtr).getName());
+                        tableModel.addRow(new Object[]{
+                                history.get(nCtr).getUsername(),
+                                history.get(nCtr).getName(),
+                                history.get(nCtr).getStock(),
+                                product.getPrice(),
+                                product.getPrice() * history.get(nCtr).getStock(),
+                                history.get(nCtr).getTimestamp()
+                        });
+                    }
+                }
+            } else if(current.getRole() == 2){
+                for(int nCtr = 0; nCtr < history.size(); nCtr++){
+                    if(searchFld.getText().contains(history.get(nCtr).getName()) && current.getUsername().equals(history.get(nCtr).getUsername()) ||
+                            history.get(nCtr).getName().contains(searchFld.getText()) && current.getUsername().equals(history.get(nCtr).getUsername())){
+
+                        Product product = sqlite.getProduct(history.get(nCtr).getName());
+                        tableModel.addRow(new Object[]{
+                                history.get(nCtr).getUsername(),
+                                history.get(nCtr).getName(),
+                                history.get(nCtr).getStock(),
+                                product.getPrice(),
+                                product.getPrice() * history.get(nCtr).getStock(),
+                                history.get(nCtr).getTimestamp()
+                        });
+                    }
                 }
             }
+
+
         }
     }//GEN-LAST:event_searchBtnActionPerformed
 
